d52b19c0134cbd989b22ebb9d7d2f58b5027d390
Helen-Noe <>
2021-02-25 23:51:18 +1100

    subclass UITableViewController

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+	
+	let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+	
+	// MARK: - TableView Datasource Methods
+	
+	// set the number of rows first
+	// populate data into each cell
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return itemArray.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for: indexPath)
+		cell.textLabel?.text = itemArray[indexPath.row]
+		print(indexPath.row)
+		return cell
+	}
+
+
+}
+
